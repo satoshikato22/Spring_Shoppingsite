d18f4413c3e7c3eec874c189085a3bcd4b81c5e3
加藤怜 <jatgadm@gmail.com>
2020-04-01 20:53:31 +0900

    動作確認済み

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,24 @@
 insert into Customer(customerId,password,customerName,birthday,age,role) values('jatgadm@gmail.com','password','加藤怜','1994-09-27',25,'ROLE_ADMIN');
+
+insert into product values(null,'まぐろ',100);
+insert into product values(null,'サーモン',100);
+insert into product values(null,'えび',100);
+ insert into product values(null,'いか',100);
+insert into product values(null,'えんがわ',100);
+insert into product values(null,'あなご',100);
+insert into product values(null,'たまご',100);
+insert into product values(null,'ほたて',100);
+insert into product values(null,'赤貝',100);
+insert into product values(null,'つぶ貝',100);
+insert into product values(null,'サラダ軍艦',150);
+insert into product values(null,'ねぎとろ軍艦',150);
+insert into product values(null,'ねぎとろ巻',150);
+insert into product values(null,'アボカド巻',150);
+insert into product values(null,'トロ',200);
+insert into product values(null,'いくら',200);
+insert into product values(null,'うに',200);
+
+insert into userinfo values(null,'さとし','Ktud3104','test@gmail.com','神奈川県鎌倉市大船');
+
+insert into log values(1,'2019-06-10','まぐろ','クレジット');
+
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,23 @@
 create table Customer(customerId varchar(50) primary key,password varchar(100),customerName varchar(50),birthday DATE,age INT,role varchar(50));
 
+create table purchase (productid int not null,productname varchar(20) not null,productprice int not null,productcount int not null,customername varchar(20) not null,customeraddress varchar(1024) not null);
+
+create table product(id int auto_increment primary key,
+name varchar(100) not null,
+price int not null);
+
+create table userinfo(
+userid int auto_increment primary key,
+name varchar(100) not null,
+pass varchar(100) not null,
+mail varchar(100) not null,
+address varchar(100) not null);
+
+create table log(
+userid int not null,
+date varchar(100) not null,
+product varchar(100) not null,
+Settlement varchar(100) not null,
+FOREIGN KEY (userid)
+REFERENCES userinfo(userid)
+);
